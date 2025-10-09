1b65d8451658d9e4340d4f8172f83ed89adb0157
AndrewJaleo <andresalvarezdelpozo@hotmail.com>
2025-10-09 13:39:04 +0200

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://mgstqiwithuocfcytzrf.supabase.co";
+const supabaseUrl = "https://mgstqiwithuocfcytzrf.supabase.co;
 // const supabaseUrl = "https://cniposhbxgcfqkwnrnhv.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1nc3RxaXdpdGh1b2NmY3l0enJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI4MTYzMDUsImV4cCI6MjA1ODM5MjMwNX0.M5p9OXB6X-8JkJeHYs37YypXeE4o5_ze_sYmEXSDsKs";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6eHB0aW9kZ2tvenhvcHJweHJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU4NTg1ODcsImV4cCI6MjA3MTQzNDU4N30._ivRbjBACdV4BOokkOhuvohYMwKKR8X4zvSHMvWkt6w";
 // const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNuaXBvc2hieGdjZnFrd25ybmh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA0NzQ3MjMsImV4cCI6MjA1NjA1MDcyM30.O2NKr-TZSFhTc2y1TiOMwUDn7r1JjVKxPaMvAysnKbU";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
